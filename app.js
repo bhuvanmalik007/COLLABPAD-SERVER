6352094048f8919bbd8ec3d68017d306bcd1032e
bhuvanmalik007 <bhuvanmalik007@gmail.com>
2016-08-04 07:08:10 +0000

    front end try

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/',function(req,res){
-    res.sendFile(__dirname+'/front/index.html');
+    res.render(__dirname+'/front/index.html');
   //res.send('collabpad');
 });
 
